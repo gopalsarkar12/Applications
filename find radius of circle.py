37aa245f82d164ba3b3928d4490fdb754b543438
gopalsarkar12 <110389254+gopalsarkar12@users.noreply.github.com>
2022-09-16 18:26:43 +0530

    Add files via upload

---
+++ find radius of circle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+from math import pi
+
+r=float(input("Enter the radius of diameter=  "))
+
+print("The diameter of radius is ", str(pi*r**2))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
